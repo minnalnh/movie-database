e5e62d2b914847687f24885965cad92861d81123
minnalnh <minnalnh@gmail.com>
2026-02-15 20:40:52 +0100

    added fisher yates function

--- scripts/modules/gui.js
+++ scripts/modules/gui.js
@@ -7,17 +7,50 @@ export function renderMovies() {
     const sectionRef = getElement('#recommendSection');
     sectionRef.innerHTML = '';
 
+    const displayedMovies = [];
+
     for(let i = 0; i < movies.length; i++) {
-        const movieRef = createMovieItem(movies[i]);
-        sectionRef.appendChild(movieRef);
+        const movieRef = createMovieItem(movies[i]); // varje film
+
+        for(let i = 10 -1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i));
+            let k = movies[i];
+            movies[i] = movies[j];
+            movies[j] = k;
+
+            displayedMovies.push(movies);
+        }
+
+        //sectionRef.appendChild(movieRef);
+    }
+
+    for(let i = 0; i < 10; i++) {
+        sectionRef.appendChild(displayedMovies[i]);
     }
 }
+/*
+function fisherYates(movies) {
+    const displayedMovies = [];
 
-function createMovieItem(movie) { // hämta ut mer info än bara titeln
-    const movieRef = createElement('p');
-    addClass(movieRef, 'recmnd-section__info');
-    movieRef.textContent = movie.Title;
-        console.log(movieRef)
+    for(let i = 10 -1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i));
+        let k = movies[i];
+        movies[i] = movies[j];
+        movies[j] = k;
+
+        displayedMovies.push(movies);
+    }
+    return displayedMovies;
+}
+*/
+function createMovieItem(movie) {
+    const movieRef = createElement('article');
+    addClass(movieRef, 'movie');
+
+    movieRef.innerHTML = `
+        <img src=${movie.Poster} alt="Poster – ${movie.Title}" class="movie__poster">
+        <h3 class="movie__title">${movie.Title}</h3>
+    `;
 
     movieRef.addEventListener('click', () => {
         // renderDetails(movie);
@@ -25,4 +58,4 @@ function createMovieItem(movie) { // hämta ut mer info än bara titeln
     });
 
     return movieRef;
-}
+} 
