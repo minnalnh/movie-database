b83ffcd0075b663bc1988e3d5236c51ee89062da
minnalnh <minnalnh@gmail.com>
2026-02-17 15:34:22 +0100

    added fetch to omdbApi

--- scripts/modules/api.js
+++ scripts/modules/api.js
@@ -2,3 +2,8 @@ export async function fetchRecommendedMovies() {
     const response = await fetch('https://santosnr6.github.io/Data/favoritemovies.json');
     return await response.json();
 }
+
+export async function fetchOmdbApi() {
+    const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=52ddd3cb');
+    return await response.json();
+}
